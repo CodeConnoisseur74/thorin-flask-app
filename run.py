838fd0f3d21aeb33e82179d4cec4a1d721b1f9d1
CodeConnoisseur74 <sherry.andrews@outlook.com>
2023-10-16 12:44:33 +0000

    add templates

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 
 # variable 'app'
@@ -11,7 +11,22 @@ app = Flask(__name__)
 # (pie-notation)
 @app.route("/")
 def index():
-    return "Hello, World"
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/careers")
+def careers():
+    return render_template("careers.html")
 
 
 # '__main__' is default module
@@ -19,4 +34,5 @@ if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
-        debug=True)
+        debug=True,
+    )
