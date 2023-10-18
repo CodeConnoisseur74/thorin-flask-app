e4b142b94ef9e78cea758e631e6dfc18aca34c7d
CodeConnoisseur74 <sherry.andrews@outlook.com>
2023-10-18 17:45:11 +0000

    add 3 paragraphs to about page

--- run.py
+++ run.py
@@ -16,17 +16,18 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html",
+                           page_title="About", list_of_numbers=[1, 2, 3])
 
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", page_title="Contact")
 
 
 @app.route("/careers")
 def careers():
-    return render_template("careers.html")
+    return render_template("careers.html", page_title="Careers")
 
 
 # '__main__' is default module
