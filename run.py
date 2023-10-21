1806d14cfa4ac5c05777b6f6c92a5726f4ee8954
CodeConnoisseur74 <sherry.andrews@outlook.com>
2023-10-21 12:10:13 +0000

    update templates and python

--- run.py
+++ run.py
@@ -1,11 +1,14 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"):
+    import env
 
 
 # variable 'app'
 # first argument of th3e Flask class is 'name'
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
 # Decorator : A way of wrapping functions
@@ -17,22 +20,34 @@ def index():
 
 @app.route("/about")
 def about():
-
     data = []
     with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
     return render_template("about.html", page_title="About", company=data)
 
 
-@app.route("/contact")
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+    return render_template("member.html", member=member)
+
+
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message!".format(
+            request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
 
 
 @app.route("/careers")
 def careers():
     return render_template("careers.html", page_title="Careers")
-
 
 
 # '__main__' is default module
